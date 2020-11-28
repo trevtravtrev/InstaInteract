031c8342d5a95af55c965c061649e516ceb0e2a9
Trevor White <trevor.white@wayne.edu>
2020-11-27 19:57:35 -0500

    Improved generator.py algorithm

--- InstaInteract/generator.py
+++ InstaInteract/generator.py
@@ -5,6 +5,8 @@ Inputs: 1. text file of words/phrases 2. text file of unicode emojis (ex: \u2714
 Output: prints new comment list to console (copy and paste it in config.comments)
 """
 
+import itertools
+
 
 def get_words(filename):
     with open(filename) as f:
@@ -13,51 +15,53 @@ def get_words(filename):
 
 
 def get_emojis(filename):
+    emoji_list = []
     with open(filename) as f:
-        emoji_list = f.read().splitlines()
+        emojis = f.read().splitlines()
+        for emoji in emojis:
+            decoded_emoji = emoji.encode().decode('unicode_escape')
+            emoji_list.append(decoded_emoji)
     return emoji_list
 
 
-def create_comment_list(words, emojis, max_emojis):
-    """
-    :param words: word list created from text file of words/phrases by get_words function
-    :param emojis: emoji list created from text file of emojis by get_emojis function
-    :param max_emojis: create every permutation of comment with 0 to max_emojis amount of emojis for each word/phrase
-    :return: list of new comments created
-    """
-    comment_list = []
+def create_permutations(words, emojis, emoji_len, word_emoji_len, emoji_perm_len=2):
+    permutations = []
+    # add all words
+    for word in words:
+        permutations.append(word)
 
-    # generate all permutations of words + emojis with 0 to max_emoji permutations per emoji (word + emoji)
-    for word in words:  # iterate through each word in words
-        comment_list.append(word)   # append word to comment list (word only)
-        for emoji in emojis:    # iterate through each emoji in emojis
-            for i in range(max_emojis): # create every permutation of word + emoji with 0 to max_emojis permutations
-                emoji_multiplier = emoji * (i+1)
-                comment = f'{word} {emoji_multiplier}'
-                decoded_comment = comment.encode().decode('unicode_escape')
-                comment_list.append(decoded_comment)    # append comment to comment list (word + emoji)
-
-    # generate all permutations of each emoji from 0 to max_emoji amount (emoji only)
+    # add all permutations of same emoji to same emoji*emoji_len
     for emoji in emojis:
-        for j in range(max_emojis):
-            emoji_multiplier = emoji * (j+1)
-            decoded_emoji_multiplier = emoji_multiplier.encode().decode('unicode_escape')
-            comment_list.append(decoded_emoji_multiplier)
+        for i in range(emoji_len):
+            permutations.append(emoji*(i+1))
 
-    return comment_list
+    # add all permutations of word + emoji*word_emoji_len
+    for word_emoji_set in itertools.product(words, emojis):
+        word_emoji_string = ' '.join(word_emoji_set)
+        emoji_stripped_sting = word_emoji_string[:-1]
+        emoji = word_emoji_string[-1]
+        for i in range(word_emoji_len):
+            permutations.append(emoji_stripped_sting+(emoji*(i+1)))
+
+    # add all permutations of unique emoji combinations of emoji_perm_len (recommended value below <3)
+    for permutation in list(itertools.permutations(emojis, emoji_perm_len)):
+        emoji_str = ''.join(permutation)
+        permutations.append(emoji_str)
+
+    print(permutations)
+    print(f'length: {len(permutations)}')
 
 
 def main():
     # settings
     words_file = "words.txt"
     emojis_file = "emojis.txt"
-    max_emojis = 3
 
     # main routine
     words = get_words(words_file)
     emojis = get_emojis(emojis_file)
-    comments = create_comment_list(words, emojis, max_emojis)
-    print(comments)
+    
+    create_permutations(words, emojis, 10, 2, 2)
     # copy the output list to config.comments
 
 
